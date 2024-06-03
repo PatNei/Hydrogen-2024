73c072b645afb126dfff26abd34fdc174c1e376d
Patrick <PatrickNeira@pm.me>
2024-06-03 14:14:57 +0000

    refactor ProductItem

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,7 @@
 import { json, type LoaderFunctionArgs } from "@shopify/remix-oxygen";
 import { useLoaderData, Link, type MetaFunction } from "@remix-run/react";
-import {
-	Pagination,
-	getPaginationVariables,
-	Money,
-	Image,
-} from "@shopify/hydrogen";
-import { generateSrcSet } from "@shopify/hydrogen-react/Image";
+import { Pagination, getPaginationVariables, Money } from "@shopify/hydrogen";
+import type { HydrogenImageProps } from "@shopify/hydrogen-react/Image";
 import type { ProductItemFragment } from "storefrontapi.generated";
 import { useVariantUrl } from "~/lib/variants";
 import {
@@ -88,16 +83,17 @@ function ProductItem({
 }) {
 	const variant = product.variants.nodes[0];
 	const variantUrl = useVariantUrl(product.handle, variant.selectedOptions);
-
 	return (
-		<div className="min-h-64 gap-2 flex max-w-screen flex-col">
+		<div className="mb-4 min-h-64 gap-10 flex max-w-screen flex-col">
 			<h4>{product.title}</h4>
 
 			<Carousel
 				opts={{
 					align: "start",
+					loop: true,
+					dragFree: true,
 				}}
-				className="flex p-4 gap-4 max-w-[100dvw] min-w-[100dvw]"
+				className="flex p-6 gap-4 max-w-[100dvw] min-w-[100dvw]"
 			>
 				<div className="content-center">
 					<CarouselPrevious disabled={product.images.nodes.length < 5} />
@@ -107,61 +103,31 @@ function ProductItem({
 						{product.images.nodes.length > 0 ? (
 							product.images.nodes.map((_image) => {
 								return (
-									<CarouselItem
-										className="sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
-										key={`ci-${_image.id}`}
-									>
-										<Link key={product.id} prefetch="intent" to={variantUrl}>
-											<AspectRatio ratio={4 / 5}>
-												<img
-													className="min-w-full min-h-full"
-													key={_image.id}
-													width={1000}
-													height={1000}
-													alt={_image.altText || product.title}
-													src={_image.url}
-												/>
-											</AspectRatio>
-										</Link>
-									</CarouselItem>
+									<CarouselProductItem
+										key=""
+										product={product}
+										variantUrl={variantUrl}
+										invisible={false}
+										_image={{ data: _image }}
+									/>
 								);
 							})
 						) : (
-							<CarouselItem
-								className="sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
-								key={"ci-placeholder"}
-							>
-								<Link key={product.id} prefetch="intent" to={variantUrl}>
-									<AspectRatio ratio={4 / 5}>
-										<img
-											className="w-full h-full"
-											key={"img-placeholder"}
-											width={1000}
-											height={1000}
-											alt={`${product.title} placeholder`}
-											src={placeholderImage}
-										/>
-									</AspectRatio>
-								</Link>
-							</CarouselItem>
+							<CarouselProductItem
+								product={product}
+								variantUrl={variantUrl}
+								invisible={false}
+							/>
 						)}
 						{Array.from({ length: 5 - product.images.nodes.length - 1 }).map(
 							(_, i) => {
 								return (
-									<CarouselItem
-										className="basis-1 sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
-										key={`ci-placeholder-${i.toString()}`}
-									>
-										<AspectRatio ratio={4 / 5}>
-											<img
-												className="min-w-full min-h-full opacity-0"
-												key={"img-placeholder"}
-												width={1000}
-												height={1000}
-												alt={`${product.title} placeholder`}
-											/>
-										</AspectRatio>
-									</CarouselItem>
+									<CarouselProductItem
+										key={`CPI-${i.toString()}`}
+										product={product}
+										variantUrl={variantUrl}
+										invisible={true}
+									/>
 								);
 							},
 						)}
@@ -171,10 +137,11 @@ function ProductItem({
 					<CarouselNext disabled={product.images.nodes.length < 5} />
 				</div>
 			</Carousel>
-
-			<small>
-				<Money data={product.priceRange.minVariantPrice} />
-			</small>
+			<div>
+				<small>
+					<Money data={product.priceRange.minVariantPrice} />
+				</small>
+			</div>
 		</div>
 	);
 }
@@ -249,3 +216,38 @@ const CATALOG_QUERY = `#graphql
   }
   ${PRODUCT_ITEM_FRAGMENT}
 ` as const;
+
+type CarouselProductItemProps = {
+	product: ProductItemFragment;
+	variantUrl: string;
+	invisible: boolean;
+	_image?: HydrogenImageProps;
+};
+
+const CarouselProductItem = ({
+	product,
+	variantUrl,
+	invisible,
+	_image,
+}: CarouselProductItemProps) => {
+	return (
+		<CarouselItem
+			className="sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
+			key={`ci-${_image?.data?.id ?? "placeholder"}`}
+		>
+			<Link key={product.id} prefetch="intent" to={variantUrl}>
+				<AspectRatio ratio={4 / 5}>
+					<img
+						className={`min-w-full min-h-full ${invisible ? "opacity-0" : ""}`}
+						key={_image?.data?.id ?? "image-placeholder"}
+						width={IMAGE_PRODUCT_WIDTH}
+						height={IMAGE_PRODUCT_HEIGHT}
+						alt={`${product.title}-${_image?.data?.altText ?? "placeholder"}`}
+						src={invisible ? "" : _image?.data?.url ?? placeholderImage}
+						aria-hidden={invisible}
+					/>
+				</AspectRatio>
+			</Link>
+		</CarouselItem>
+	);
+};
